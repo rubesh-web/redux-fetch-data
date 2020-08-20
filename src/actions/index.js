ec64a52d1d9f05a910b6dce7b74f1df0020b567b
RUBESH <rubeshkriz@gmail.com>
2020-08-20 20:48:10 +0530

    fetched data API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,11 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = () => {
-  return async function (dispatch, getState) {
-    const response = await jsonPlaceholder.get("/posts");
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceholder.get("/posts");
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
+};
 
-    dispatch({ type: "FETCH_POSTS", payload: response });
-  };
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({ type: "FETCH_USER", payload: response.data });
 };
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,15 +2,35 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { fetchPosts } from "../actions";
+import UserHeader from "./UserHeader";
 
 class PostList extends React.Component {
-  // componentDidMount() {
-  //   this.props.fetchPosts();
-  // }
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  renderList() {
+    return this.props.posts.map((post) => {
+      return (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.title}</p>
+          <UserHeader userId={post.userId} />
+        </div>
+      );
+    });
+  }
 
   render() {
-    return <div>Post List</div>;
+    console.log(this.props.posts);
+    return <div>{this.renderList()}</div>;
   }
 }
 
-export default connect(null, fetchPosts)(PostList);
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts,
+  };
+};
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
